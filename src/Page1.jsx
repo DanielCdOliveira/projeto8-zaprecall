648bba7b3daf22269e6145df33d6cf4f8cbc6cd0
Daniel Costa de Oliveira <danielcoliv98@gmail.com>
2022-03-16 15:11:47 -0300

    Perguntas exibidas

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,10 +1,14 @@
+import QuestionCreator from "./QuestionCreator";
+
 export default function Page1() {
   return (
     <main>
       <header>
-          <img src="assets/img/logo-pequeno.png" alt="logo header" />
-          <h2>ZapRecall</h2>
+        <img src="assets/img/logo-pequeno.png" alt="logo header" />
+        <h2>ZapRecall</h2>
       </header>
+      <QuestionCreator />
+      
     </main>
   );
 }
